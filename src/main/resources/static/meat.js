6b8d33e84d0fbf559d9dd23bf915387ec2f77c43
lowellmparker <lowellmparker@gmail.com>
2019-02-14 11:01:28 -0500

    final

--- src/main/resources/static/meat.js
+++ src/main/resources/static/meat.js
@@ -19,42 +19,20 @@ makeAjaxGet("http://localhost:9595/flight/min", getMin);
 makeAjaxGet("http://localhost:9595/flight/sum", getTickPrice);
 addRow1();
 function getAvg(xhrObj_3){
-documemnt.getElementById("min") = JSON.parse(xhrObj_3.response);
+document.getElementById("min").innerHTML += JSON.parse(xhrObj_3.response);
 }
 function getMax(xhrObj_4){
-	documemnt.getElementById("med") = JSON.parse(xhrObj_4.response);
+	document.getElementById("med").innerHTML += JSON.parse(xhrObj_4.response);
 }    		
 function getMed(xhrObj_5){
-	documemnt.getElementById("max")  = JSON.parse(xhrObj_5.response);
+	document.getElementById("max").innerHTML  += JSON.parse(xhrObj_5.response);
 }
 function getMin(xhrObj_6){
-	documemnt.getElementById("avg") = JSON.parse(xhrObj_6.response);
+	document.getElementById("avg").innerHTML += JSON.parse(xhrObj_6.response);
 }
 function getTickPrice(xhrObj_1){
-document.getElementById("total") = JSON.parse(xhrObj_1.response);
+document.getElementById("total").innerHTML += JSON.parse(xhrObj_1.response);
 }
-function addRow1( ){	
-	document.getElementsById("stats")[0].appendChild(row);
-	let row = document.createElement("tr");
-	let cell1 = document.createElement("td");
-	let cell2 = document.createElement("td");
-	let cell3 = document.createElement("td");
-	let cell4 = document.createElement("td");
-	
-	row.appendChild(cell1);
-	row.appendChild(cell2);
-	row.appendChild(cell3);
-    row.appendChild(cell4);
-    
-    
-	cell1.innerHTML = min;
-	cell2.innerHTML = max;
-	cell3.innerHTML = avg;
-    cell4.innerHTML = TicketPrice;
-}
-    
-    
-
 
 
 //makeAjaxGet("http://localhost:9595/customer/all/totalsales", getCustTot)
